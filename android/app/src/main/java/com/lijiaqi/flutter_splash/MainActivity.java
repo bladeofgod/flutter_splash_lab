fef1338448a054af20d841e7345d5ab03f41c4ad
bladeofgod <450103187@qq.com>
2021-01-02 10:01:15 +0800

    正常

--- android/app/src/main/java/com/lijiaqi/flutter_splash/MainActivity.java
+++ android/app/src/main/java/com/lijiaqi/flutter_splash/MainActivity.java
@@ -41,6 +41,10 @@ import com.facebook.drawee.controller.ControllerListener;
 import com.facebook.drawee.interfaces.DraweeController;
 import com.facebook.drawee.view.SimpleDraweeView;
 import com.facebook.imagepipeline.animated.base.AbstractAnimatedDrawable;
+import com.opensource.svgaplayer.SVGADrawable;
+import com.opensource.svgaplayer.SVGAImageView;
+import com.opensource.svgaplayer.SVGAParser;
+import com.opensource.svgaplayer.SVGAVideoEntity;
 
 import java.util.concurrent.ExecutionException;
 
@@ -64,6 +68,7 @@ public class MainActivity extends FlutterActivity {
             public void run() {
                 Log.i("child thread", "init -------");
                 Looper.prepare();
+
                 WindowManager wm = (WindowManager)getSystemService(WINDOW_SERVICE);
 //                DraweeController controller = Fresco.newDraweeControllerBuilder()
 //                        .setUri(Uri.parse(webUrl))
@@ -74,80 +79,103 @@ public class MainActivity extends FlutterActivity {
 //                SimpleDraweeView simpleDraweeView = new SimpleDraweeView(MainActivity.this);
 //                simpleDraweeView.setController(controller);
                 //simpleDraweeView.setImageResource(R.drawable.lion);
-                ImageView imageView = new ImageView(MainActivity.this);
-                imageView.setBackgroundColor(getResources().getColor(R.color.red));
-                WindowManager.LayoutParams params = new WindowManager.LayoutParams();
-                params.width = wm.getDefaultDisplay().getWidth();
-                params.height = wm.getDefaultDisplay().getHeight();
-
-                wm.addView(imageView, params);
-                Bitmap bitmap;
-                RequestOptions options = new RequestOptions();
-                options.diskCacheStrategy(DiskCacheStrategy.ALL);
-                final GifDrawable[] drawable = new GifDrawable[1];
+//                ImageView imageView = new ImageView(MainActivity.this);
+//                imageView.setBackgroundColor(getResources().getColor(R.color.red));
+                SVGAImageView imageView = new SVGAImageView(MainActivity.this);
                 Handler handler = new Handler(){
                     @Override
                     public void handleMessage(Message msg) {
                         switch (msg.what){
                             case 1111:
                                 Log.i("glide-------", "msg");
-                                imageView.setImageDrawable(drawable[0]);
+                                imageView.setImageDrawable((SVGADrawable)msg.obj);
+                                imageView.startAnimation();
                                 break;
                         }
                     }
                 };
+                SVGAParser parser = new SVGAParser(getApplicationContext());
+                parser.decodeFromAssets("Goddess.svga", new SVGAParser.ParseCompletion() {
+                    @Override
+                    public void onComplete(SVGAVideoEntity svgaVideoEntity) {
+                        SVGADrawable drawable = new SVGADrawable(svgaVideoEntity);
+                        Message msg = Message.obtain();
+                        msg.what = 1111;
+                        msg.obj = drawable;
+                        handler.sendMessage(msg);
+                    }
 
-                try {
-                      Glide.with(MainActivity.this)
-                            .asGif()
-                            .diskCacheStrategy(DiskCacheStrategy.ALL)
-                            .load(R.drawable.lion)
-                             .listener(new RequestListener<GifDrawable>() {
-                                 @Override
-                                 public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<GifDrawable> target, boolean isFirstResource) {
-                                     return false;
-                                 }
+                    @Override
+                    public void onError() {
 
-                                 @Override
-                                 public boolean onResourceReady(GifDrawable resource, Object model, Target<GifDrawable> target, DataSource dataSource, boolean isFirstResource) {
-                                     Log.i("--------", "ready");
-                                     if(!resource.isRunning()){
-                                         resource.start();
-                                     }
+                    }
+                });
 
-//                                     Message msg = Message.obtain();
-//                                     msg.what = 1111;
-//                                     handler.sendMessage(msg);
-                                     //imageView.setImageDrawable(getDrawable(R.drawable.lion));
-                                     //imageView.setImageDrawable(resource.getCurrent());
-                                     return true;
-                                 }
-                             }).submit().get();
-//                             .listener(new RequestListener<Bitmap>() {
+
+                WindowManager.LayoutParams params = new WindowManager.LayoutParams();
+                params.width = wm.getDefaultDisplay().getWidth();
+                params.height = wm.getDefaultDisplay().getHeight();
+
+                wm.addView(imageView, params);
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        wm.removeView(imageView);
+                    }
+                }, 6000);
+
+//                Bitmap bitmap;
+//                RequestOptions options = new RequestOptions();
+//                options.diskCacheStrategy(DiskCacheStrategy.ALL);
+//                final GifDrawable[] drawable = new GifDrawable[1];
+
+//
+//                try {
+//                      Glide.with(MainActivity.this)
+//                            .asGif()
+//                            .diskCacheStrategy(DiskCacheStrategy.ALL)
+//                            .load(R.drawable.lion)
+//                             .listener(new RequestListener<GifDrawable>() {
 //                                 @Override
-//                                 public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Bitmap> target, boolean isFirstResource) {
+//                                 public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<GifDrawable> target, boolean isFirstResource) {
 //                                     return false;
 //                                 }
 //
 //                                 @Override
-//                                 public boolean onResourceReady(Bitmap resource, Object model, Target<Bitmap> target, DataSource dataSource, boolean isFirstResource) {
+//                                 public boolean onResourceReady(GifDrawable resource, Object model, Target<GifDrawable> target, DataSource dataSource, boolean isFirstResource) {
+//                                     Log.i("--------", "ready");
+//                                     if(!resource.isRunning()){
+//                                         resource.start();
+//                                     }
 //
-//                                     imageView.setImageBitmap(resource);
-//                                     return false;
+////                                     Message msg = Message.obtain();
+////                                     msg.what = 1111;
+////                                     handler.sendMessage(msg);
+//                                     imageView.setImageDrawable(getDrawable(R.drawable.lion));
+//                                     //imageView.setImageDrawable(resource.getCurrent());
+//                                     return true;
 //                                 }
-//                             })
+//                             }).submit().get();
+////                             .listener(new RequestListener<Bitmap>() {
+////                                 @Override
+////                                 public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Bitmap> target, boolean isFirstResource) {
+////                                     return false;
+////                                 }
+////
+////                                 @Override
+////                                 public boolean onResourceReady(Bitmap resource, Object model, Target<Bitmap> target, DataSource dataSource, boolean isFirstResource) {
+////
+////                                     imageView.setImageBitmap(resource);
+////                                     return false;
+////                                 }
+////                             })
+//
+//                     //imageView.setImageBitmap(bitmap);
+//
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
 
-                     //imageView.setImageBitmap(bitmap);
-
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-//                new Handler().postDelayed(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        wm.removeView(simpleDraweeView);
-//                    }
-//                }, 2000);
                 Looper.loop();
 
 
