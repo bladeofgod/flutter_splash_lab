edcd46ea0d2269ca82bbcdf0366c6490f3da8ea2
bladeofgod <450103187@qq.com>
2021-01-02 15:18:39 +0800

    ...

--- android/app/src/main/java/com/lijiaqi/flutter_splash/MainActivity.java
+++ android/app/src/main/java/com/lijiaqi/flutter_splash/MainActivity.java
@@ -57,7 +57,7 @@ public class MainActivity extends FlutterActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
-        init();
+        initSplashPage();
         super.onCreate(savedInstanceState);
 
         //addView();
@@ -90,27 +90,15 @@ public class MainActivity extends FlutterActivity {
 
 
     final String webUrl = "https://isparta.github.io/compare-webp/image/gif_webp/webp/1.webp";
-    public void init(){
+    public void initSplashPage(){
         new Thread(new Runnable() {
             @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
             @Override
             public void run() {
-                Log.i("child thread", "init -------");
                 Looper.prepare();
-
                 WindowManager wm = (WindowManager)getSystemService(WINDOW_SERVICE);
-//                DraweeController controller = Fresco.newDraweeControllerBuilder()
-//                        .setUri(Uri.parse(webUrl))
-//                        .setAutoPlayAnimations(true)
-//                        .build();
-
-//
-//                SimpleDraweeView simpleDraweeView = new SimpleDraweeView(MainActivity.this);
-//                simpleDraweeView.setController(controller);
-                //simpleDraweeView.setImageResource(R.drawable.lion);
-//                ImageView imageView = new ImageView(MainActivity.this);
-//                imageView.setBackgroundColor(getResources().getColor(R.color.red));
                 SVGAImageView imageView = new SVGAImageView(getApplicationContext());
+                imageView.setBackgroundColor(getResources().getColor(R.color.white));
                 Handler handler = new Handler(){
                     @Override
                     public void handleMessage(Message msg) {
@@ -140,7 +128,6 @@ public class MainActivity extends FlutterActivity {
                     }
                 });
 
-
                 WindowManager.LayoutParams params = new WindowManager.LayoutParams();
                 params.width = wm.getDefaultDisplay().getWidth();
                 params.height = wm.getDefaultDisplay().getHeight();
@@ -153,57 +140,6 @@ public class MainActivity extends FlutterActivity {
                     }
                 }, 6000);
 
-//                Bitmap bitmap;
-//                RequestOptions options = new RequestOptions();
-//                options.diskCacheStrategy(DiskCacheStrategy.ALL);
-//                final GifDrawable[] drawable = new GifDrawable[1];
-
-//
-//                try {
-//                      Glide.with(MainActivity.this)
-//                            .asGif()
-//                            .diskCacheStrategy(DiskCacheStrategy.ALL)
-//                            .load(R.drawable.lion)
-//                             .listener(new RequestListener<GifDrawable>() {
-//                                 @Override
-//                                 public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<GifDrawable> target, boolean isFirstResource) {
-//                                     return false;
-//                                 }
-//
-//                                 @Override
-//                                 public boolean onResourceReady(GifDrawable resource, Object model, Target<GifDrawable> target, DataSource dataSource, boolean isFirstResource) {
-//                                     Log.i("--------", "ready");
-//                                     if(!resource.isRunning()){
-//                                         resource.start();
-//                                     }
-//
-////                                     Message msg = Message.obtain();
-////                                     msg.what = 1111;
-////                                     handler.sendMessage(msg);
-//                                     imageView.setImageDrawable(getDrawable(R.drawable.lion));
-//                                     //imageView.setImageDrawable(resource.getCurrent());
-//                                     return true;
-//                                 }
-//                             }).submit().get();
-////                             .listener(new RequestListener<Bitmap>() {
-////                                 @Override
-////                                 public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Bitmap> target, boolean isFirstResource) {
-////                                     return false;
-////                                 }
-////
-////                                 @Override
-////                                 public boolean onResourceReady(Bitmap resource, Object model, Target<Bitmap> target, DataSource dataSource, boolean isFirstResource) {
-////
-////                                     imageView.setImageBitmap(resource);
-////                                     return false;
-////                                 }
-////                             })
-//
-//                     //imageView.setImageBitmap(bitmap);
-//
-//                } catch (Exception e) {
-//                    e.printStackTrace();
-//                }
 
                 Looper.loop();
 
